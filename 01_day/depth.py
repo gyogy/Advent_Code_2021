e5aabf2f338fd1e8a4f95e180ae7304af4dbd4f0
gyogy <gmkukov@gmail.com>
2021-12-01 13:01:52 +0200

    Day 1 solutions

---
+++ 01_day/depth.py
@@ -0,0 +1,17 @@
+with open('input', 'r') as f:
+    raw_string = f.read()
+
+str_values = raw_string.split('\n')
+
+for i in range(str_values.count('')):
+    str_values.remove('')
+
+values = list(map(int, str_values))
+
+sums = []
+for i in range(len(values)-2):
+    sums.append(sum(values[i:i+3]))
+
+count_of_increased_values = sum(a < b for a, b in zip(values[:-1], values[1:]))
+count_of_increased_sums = sum(a < b for a, b in zip(sums[:-1], sums[1:]))
+print(count_of_increased_sums)
