e4a3552ab834151e540566ba6c9f8c99a4fafb05
gyogy <gmkukov@gmail.com>
2021-12-02 11:38:35 +0200

    Refactored day 2 solution

--- 02/mover.py
+++ 02/mover.py
@@ -5,20 +5,31 @@ def parse_input(filename):
         return steps
 
 def part_01(steps):
-    fw = [step[-1] for step in steps if step[0] == 'f']
-    dn = [step[-1] for step in steps if step[0] == 'd']
-    up = [step[-1] for step in steps if step[0] == 'u']
+    fw = [int(step[-1]) for step in steps if step[0] == 'f']
+    dn = [int(step[-1]) for step in steps if step[0] == 'd']
+    up = [int(step[-1]) for step in steps if step[0] == 'u']
     return sum(fw) * (sum(dn) - sum(up))
 
-def part_02():
+def part_02(steps):
+    x = 0; aim = 0; depth = 0;
+
     for step in steps:
-        if step[0] == 'f':
-            x += int(step[-1])
-            depth += aim * int(step[-1])
-        if step[0] == 'd':
-            aim += int(step[-1])
-        if step[0] == 'u':
-            aim -= int(step[-1])
+        dir = step[0]
+        magn = int(step[-1])
+
+        if dir == 'f':
+            x += magn
+            depth += aim * magn
+        if dir == 'd':
+            aim += magn
+        if dir == 'u':
+            aim -= magn
+    return x * depth
+
+def main(filename):
+    steps = parse_input(filename)
+    print(f'Part 1: Final position is {part_01(steps)}.')
+    print(f'Part 2: Final position is {part_02(steps)}.')
 
 if __name__ == "__main__":
-    print(__file__)
+    main('/home/gyogy/code/advent/inputs/02')
