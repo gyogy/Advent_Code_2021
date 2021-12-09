966d2a10834f51144eea957f4bd98a59d3b00e29
gyogy <gmkukov@gmail.com>
2021-12-09 23:25:08 +0200

    Day 9, solution to part 1

--- 09/09.py
+++ 09/09.py
@@ -20,29 +20,22 @@ def diagonal(row, col, i, j):
 
 
 def neighbours(matrix, row, col):
-    return [matrix[i][j] for i in range(row - 1, row + 2) for j in range(col - 1, col + 2) if in_bounds(matrix, i, j) and not diagonal(row, col, i, j)]
-
-
-def has_lowpoint(neighbours):
-    target = neighbours[-3:-2][0]
-    if target == min(neighbours):
-        return True
-    return False
-
+    n = [matrix[i][j] for i in range(row - 1, row + 2) for j in range(col - 1, col + 2) if in_bounds(matrix, i, j) and not diagonal(row, col, i, j)]
+    if matrix[row][col] == min(n) and n.count(matrix[row][col]) == 1:
+        return min(n) + 1
+    else:
+        return 0
 
 def part1(reads):
     lowpoints = []
     for row in range(len(reads)):
         for col in range(len(reads[0])):
-            n = neighbours(reads, row, col)
-            if has_lowpoint(n):
-                lowpoints.append(n)
-    ipdb.set_trace()
-    return lowpoints
+            lowpoints.append(neighbours(reads, row, col))
+    return sum(lowpoints)
 
 
 if __name__ == '__main__':
-    file = '../inputs/test'
+    file = '../inputs/09'
     reads = parse(file)
     print(part1(reads))
 
