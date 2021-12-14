6bfe29169a7632e2c2f25fc4e609af1dd31648c9
gyogy <gmkukov@gmail.com>
2021-12-14 14:51:55 +0200

    Day 10, part 2 solution

--- 10/nav.py
+++ 10/nav.py
@@ -3,12 +3,19 @@ import ipdb
 OB = '([{<'
 CB = ')]}>'
 
-points = {
+invalid_points = {
     None: 0,
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137
+}
+
+incomplete_points = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4 
 }
 
 def parse(file):
@@ -33,16 +40,51 @@ def validate(chunk):
     return
 
 
+def get_incomplete(chunk):
+    stack = []
+    for bracket in chunk:
+        try:
+            target = OB.index(stack[-1])
+        except IndexError:
+            target = None
+
+        if bracket in OB:
+            stack.append(bracket)
+        elif CB.index(bracket) == target:
+            stack.pop()
+        else:
+            return
+    stack.reverse()
+    return stack 
+
+
+def compute(stack):
+    result = 0
+    for bracket in stack:
+        result *= 5
+        result +=  incomplete_points[bracket]
+    return result
+
+
 def part1(data):
     result = 0
     for chunk in data:
-        result += points[validate(chunk)]
+        result += invalid_points[validate(chunk)]
     return result
 
+
+def part2(data):
+    stacks = [get_incomplete(chunk)
+              for chunk in data if get_incomplete(chunk) is not None]
+    scores = [compute(stack) for stack in stacks]
+    scores.sort()
+    middle_score = round(len(scores)/2) - 1
+    return scores[middle_score]
 
 if __name__ == '__main__':
     #file = '../inputs/test'
     file = '../inputs/10'
     data = parse(file)
     print(part1(data))
+    print(part2(data))
 
